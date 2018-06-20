07ae98ec32c3c12858309883fff2d7770218e80a
Tiberiu Nanea <tbnanea@gmail.com>
2018-06-20 15:01:51 +0300

    fix connection string

--- Ads/Database/Adverts.Designer.cs
+++ Ads/Database/Adverts.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Personal\Licenta\Ads\Database\Adverts.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Git\Licenta\Ads\Database\Adverts.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
